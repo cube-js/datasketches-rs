020077c7d533cb2f3fa6fa7f8ec9dc8f987f6c19
Dmitry Patsura <talk@dmtry.me>
2024-05-06 17:52:55 +0200

    feat: Support HLL
    chore: working
    
    chore: deserialize
    
    chore: working
    
    chore: working
    
    chore: more tests
    
    chore: support target hll type
    
    chore: support target hll type for new sketch

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,4 @@ pub mod stream_reducer;
 mod wrapper;
 
 pub use error::DataSketchesError;
-pub use wrapper::CpcSketch;
-pub use wrapper::CpcUnion;
-pub use wrapper::HhSketch;
-pub use wrapper::StaticThetaSketch;
-pub use wrapper::ThetaIntersection;
-pub use wrapper::ThetaSketch;
-pub use wrapper::ThetaUnion;
+pub use wrapper::*;
